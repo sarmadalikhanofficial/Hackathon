ca00f3e9ccaf8fe2f1358820626e18180e8d5d08
sarmadalikhanofficial <132395342+sarmadalikhanofficial@users.noreply.github.com>
2024-11-07 09:35:27 -0800

    Update script.js

--- milestone03/script.js
+++ milestone03/script.js
@@ -46,7 +46,7 @@ form3.addEventListener('submit', function (event) {
     var experience = document.getElementById('experience').value;
     var skills = document.getElementById('skills').value;
     //gen resume
-    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Info</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n    \n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n    \n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n    \n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n    \n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n\n    ");
+    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Info</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n    \n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n    \n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n    \n    \n\n\n    ");
     //display gen resume
     if (resumeDisplayElement1) {
         resumeDisplayElement1.innerHTML = resumeHTML;
